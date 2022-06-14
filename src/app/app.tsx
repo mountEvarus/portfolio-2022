0974d3ec4b01a30504e9a66843f1f1e0799c65d3
Evan Hynes <evan.hynes@glimpseprotocol.io>
2022-06-14 16:54:44 +0100

    feat: add default styling

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,14 +1,20 @@
 import * as React from "react"
 
+import { CssBaseline, ThemeProvider } from "@mui/material"
 import { Root } from "@src/root"
 import { BrowserRouter } from "react-router-dom"
+
+import { theme } from "./theme"
 
 export function App(): JSX.Element {
   return (
     <React.StrictMode>
-      <BrowserRouter>
-        <Root />
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <Root />
+        </BrowserRouter>
+      </ThemeProvider>
     </React.StrictMode>
   )
 }
