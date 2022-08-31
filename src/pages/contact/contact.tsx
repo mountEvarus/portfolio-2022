b011563e7afd20a16691c8667414114765d3a74a
Evan Hynes <evan.hynes@glimpseprotocol.io>
2022-08-31 17:35:42 +0100

    feat: add cv to contact page

--- src/pages/contact/contact.tsx
+++ src/pages/contact/contact.tsx
@@ -3,7 +3,7 @@ import * as React from "react"
 import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 import { faEnvelope, faFilePdf } from "@fortawesome/free-solid-svg-icons"
 import { Box, Typography } from "@mui/material"
-import PlaceholderPdf from "@src/assets/placeholder.pdf"
+import CvPdf from "@src/assets/Evan_Hynes_CV_2022.pdf"
 import { IconLink } from "@src/common/icon-link"
 import { motion } from "framer-motion"
 
@@ -30,7 +30,7 @@ export function Contact(): JSX.Element {
       <Typography variant="h2">CV</Typography>
       <Typography sx={paragraphStyles}>Feel free to look through my CV for more information about my education, experience & skills</Typography>
       <Box sx={linkBoxStyles}>
-        <IconLink download icon={faFilePdf} link={PlaceholderPdf} />
+        <IconLink download icon={faFilePdf} link={CvPdf} />
       </Box>
     </Box>
   )
